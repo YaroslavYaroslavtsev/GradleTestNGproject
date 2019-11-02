21358d68ac5241a345f84f367fa7cff3a6b81c4b
YaroslavYaroslavtsev <yaroslav.yaroslavtsev@nobitlost.com>
2019-11-03 01:45:46 +0700

    Base Tests Added

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,11 +1,18 @@
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 
+@Epic("Epic")
+@Feature("Feature")
 public class ExampleTest {
 
     @Test
+    @Story("Тест 1")
     public void testOne() {
         assertThat(true)
                 .as("AssertTrue")
@@ -13,10 +20,27 @@ public class ExampleTest {
     }
 
     @Test
+    @Story("Тест 2")
     public void testTwo() {
+        step1();
         assertThat(false)
                 .as("AssertFalse")
                 .isFalse();
+    }
+
+    @Step("Шаг 1")
+    void step1() {
+        step2();
+    }
+
+    @Step("Шаг 2")
+    void step2() {
+        step3();
+    }
+
+    @Step("Шаг 3")
+    void step3() {
+
     }
 
 }
